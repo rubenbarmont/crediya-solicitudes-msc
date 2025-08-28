fb82e54ff8ed2282e16f8a7d3ae532278ed723a0
Ruben Dario Barrios Montealegre <rubenbarmont@gmail.com>
2025-08-28 14:57:30 -0500

    feature(HU_#02): Add models and uses case

--- domain/model/src/main/java/co/com/crediya/model/solicitud/gateways/SolicitudRepository.java
+++ domain/model/src/main/java/co/com/crediya/model/solicitud/gateways/SolicitudRepository.java
@@ -1,4 +1,9 @@
 package co.com.crediya.model.solicitud.gateways;
 
+import co.com.crediya.model.solicitud.Solicitud;
+import reactor.core.publisher.Mono;
+
 public interface SolicitudRepository {
+    Mono<Solicitud> save(Solicitud solicitud);
+    Mono<Solicitud> findById(Long idSolicitud);
 }
